055b05c344ebaeec15b02c25b141011dc5ba6efc
Rodrigo Rezende <rarezende1@gmail.com>
2016-05-03 14:20:10 -0400

    Inclusion of plot1.png

--- plot1.R
+++ plot1.R
@@ -1,3 +1,11 @@
+#----------------------------------------------------------#
+#            Exploratory Data Analysis Course
+#
+# Course Project 1
+# Script for generating Plot 1
+#----------------------------------------------------------#
+
+# Reads power comsuption data from file
 fileName <- "./Data/household_power_consumption.txt"
 
 varNames <- c("Date",
@@ -16,15 +24,20 @@ powerCons<-read.csv(fileName,
                     skip = 66636,
                     nrows = 2880)
 
+# Converts Date and Time variables to Date/Time classes in R
 powerCons$Time <- paste(powerCons$Date, powerCons$Time)
-
 powerCons$Time <- strptime(powerCons$Time, format = "%d/%m/%Y %H:%M:%S")
-
 powerCons$Date <- strptime(powerCons$Date, format = "%d/%m/%Y")
 
-hist(powerCons$Global_active_power)
+# Plots to file 
+png("plot1.png")
 
-plot(powerCons$Time, powerCons$Global_active_power)
+hist(powerCons$Global_active_power, 
+     col = "red", 
+     xlab = "Global Active Power (kilowatts)",
+     main = "Global Active Power")
+
+dev.off()
 
 
 
