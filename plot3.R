16ff325ce4ff4c354e91a38620e24edc61fa396e
Rodrigo Rezende <rarezende1@gmail.com>
2016-05-03 15:02:07 -0400

    Inclusion of plot3.png

--- plot3.R
+++ plot3.R
@@ -1,3 +1,11 @@
+#----------------------------------------------------------#
+#            Exploratory Data Analysis Course
+#
+# Course Project 1
+# Script for generating Plot 3
+#----------------------------------------------------------#
+
+# Reads power comsuption data from file
 fileName <- "./Data/household_power_consumption.txt"
 
 varNames <- c("Date",
@@ -16,24 +24,31 @@ powerCons<-read.csv(fileName,
                     skip = 66636,
                     nrows = 2880)
 
+# Converts Date and Time variables to Date/Time classes in R
 powerCons$Time <- paste(powerCons$Date, powerCons$Time)
-
 powerCons$Time <- strptime(powerCons$Time, format = "%d/%m/%Y %H:%M:%S")
-
 powerCons$Date <- strptime(powerCons$Date, format = "%d/%m/%Y")
 
-windows()
+# Plots to file
+png("plot3.png")
 
-with(powerCons, plot(Time, Sub_metering_1, type = "n"))
+with(powerCons, plot(Time, 
+                     Sub_metering_1, 
+                     type = "n",
+                     xlab = "", 
+                     ylab = "Energy sub metering"))
 
-with(powerCons, lines(Time, Sub_metering_1, col="black"))
 
-with(powerCons, lines(Time, Sub_metering_2, col="red"))
+with(powerCons, {
+    lines(Time, Sub_metering_1, col="black")
+    lines(Time, Sub_metering_2, col="red")
+    lines(Time, Sub_metering_3, col="blue")
+})
 
-with(powerCons, lines(Time, Sub_metering_3, col="blue"))
-
-legend("topright", lty = 1, col= c("black", "red", "blue"), 
+legend("topright", 
+       lty = 1, 
+       col= c("black", "red", "blue"), 
        legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 
-
+dev.off()
